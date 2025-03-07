a5bf614547c3757ac7f83b8d1e0c3e8d0bc67039
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2025-03-07 15:58:34 +0330

    feat: Initialize envconfig-derive crate with FromEnv macro implementation

---
+++ envconfig-derive/src/lib.rs
@@ -0,0 +1,70 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{DeriveInput, parse_macro_input};
+
+#[proc_macro_derive(FromEnv, attributes(env))]
+pub fn derive_from_env(input: TokenStream) -> TokenStream {
+    // Parse the input (the struct definition) into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let the_struct = match input.data {
+        syn::Data::Struct(ref data) => data,
+        _ => panic!("FromEnv can only be derived for structs"),
+    };
+
+    let fields = match &the_struct.fields {
+        syn::Fields::Named(named_fields) => &named_fields.named,
+        _ => panic!("FromEnv can only be derived for structs with named fields"),
+    };
+
+    // Generate initialization code for each field
+    let field_inits: Vec<_> = fields
+        .iter()
+        .map(|field| {
+            // Get the field name (e.g., `host`)
+            match field.ident.as_ref() {
+                Some(field_ident) => {
+                    // Find the `#[env("VAR")]` attribute
+                    let env_attr = field
+                        .attrs
+                        .iter()
+                        .find(|attr| attr.path().is_ident("env"))
+                        .expect("Each field must have an #[env] attribute");
+
+
+                    let env_var = match &env_attr.meta {
+                        syn::Meta::List(l) => &l.tokens,
+                        syn::Meta::NameValue(_nv) => & quote! {},
+                        syn::Meta::Path(_p) => & quote! {},
+                    };
+
+
+                    quote! {
+                        #field_ident: std::env::var(#env_var).expect(&format!("Environment variable {} not set", #env_var))
+                    }
+                }
+                _ => {
+                    quote! {}
+                }
+            }
+        })
+        .collect();
+
+    // Get the struct name (e.g., `Config`)
+    let struct_name = input.ident;
+
+    // Generate the trait implementation
+    let expanded = quote! {
+        // The generated impl.
+        impl envconfig::FromEnv for #struct_name {
+            fn from_env() -> Self {
+                Self {
+                    #(#field_inits),* // Expand the list of field initializations
+                }
+            }
+        }
+    };
+
+    // Hand the output tokens back to the compiler.
+    TokenStream::from(expanded)
+}
